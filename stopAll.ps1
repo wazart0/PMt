fd7d8e2e60a28425527195211847a40b4503f7cb
Artur Waz <arturwaz11@gmail.com>
2019-08-10 16:45:31 +0200

    update

--- stopAll.ps1
+++ stopAll.ps1
@@ -1,4 +1,4 @@
 docker-compose.exe -f ./frontend/pmt-front/docker-compose.yml -p pmt-frontend down
-docker-compose.exe -f ./backend/docker-compose_release.yml -p pmt-backend_release down
-docker-compose.exe -f ./backend/docker-compose_develop.yml -p pmt-backend_develop down
-docker-compose.exe -f ./db/docker-compose.yml -p pmt-db down
+docker-compose.exe -f ./backend/docker-compose_release.yml -p pmt down
+docker-compose.exe -f ./backend/docker-compose_develop.yml -p pmt down
+docker-compose.exe -f ./db/docker-compose.yml -p pmt down

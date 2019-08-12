c9de7ac0b6a40a085cfc49eb3c49daaf4bcd7501
Artur Waz <arturwaz11@gmail.com>
2019-08-12 16:53:08 +0200

    improving initialization, adding pgadmin

--- runBackendDevelop.ps1
+++ runBackendDevelop.ps1
@@ -1,3 +1,5 @@
 docker-compose.exe -f ./backend/docker-compose_develop.yml -p pmt down
 docker-compose.exe -f ./backend/docker-compose_develop.yml -p pmt build
-docker-compose.exe -f ./backend/docker-compose_develop.yml -p pmt up
+docker-compose.exe -f ./backend/docker-compose_develop.yml -p pmt up -d
+
+docker.exe logs -f -t pmt_backend_1
--- stopAll.ps1
+++ stopAll.ps1
@@ -1,4 +1,5 @@
 docker-compose.exe -f ./frontend/pmt-front/docker-compose.yml -p pmt-frontend down
 docker-compose.exe -f ./backend/docker-compose_release.yml -p pmt down
 docker-compose.exe -f ./backend/docker-compose_develop.yml -p pmt down
+docker-compose.exe -f ./backend/docker-compose_pgadmin.yml -p pmt down
 docker-compose.exe -f ./db/docker-compose.yml -p pmt down

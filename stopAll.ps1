0b3409eada980bbd52c4446d73070093b972a3e0
Artur Waz <arturwaz11@gmail.com>
2019-08-16 18:59:21 +0200

    added pgadmin, login: ad, pass: ad

--- stopAll.ps1
+++ stopAll.ps1
@@ -1,5 +1,6 @@
 docker-compose.exe -f ./frontend/pmt-front/docker-compose.yml -p pmt-frontend down
 docker-compose.exe -f ./backend/docker-compose_release.yml -p pmt down
 docker-compose.exe -f ./backend/docker-compose_develop.yml -p pmt down
-docker-compose.exe -f ./backend/docker-compose_pgadmin.yml -p pmt down
+docker-compose.exe -f ./db/docker-compose_pgadmin.yml -p pmt down
 docker-compose.exe -f ./db/docker-compose.yml -p pmt down
+docker.exe network rm pmt_network

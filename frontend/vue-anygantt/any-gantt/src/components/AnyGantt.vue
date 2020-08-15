a85ba55b0a93a70f2de22bafbe1148c485e83e9e
Adrian Waz <wazadrian@gmail.com>
2020-08-15 14:00:31 +0200

    gantt index version

---
+++ frontend/vue-anygantt/any-gantt/src/components/AnyGantt.vue
@@ -0,0 +1,12 @@
+<template>
+	<div id="container" style="width: 1600px; height: 700px;"></div>
+</template>
+
+<script>
+export default {
+	name: 'AnyGantt',
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss"></style>

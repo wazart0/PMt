cf3be9584af1e08631c444ff250c9d99b17786f0
Artur Waz <awaz@ownedoutcomes.com>
2020-09-02 21:12:55 +0200

    trial with anychart

--- frontend/vue-anygantt/any-gantt/src/components/AnyGantt.vue
+++ frontend/vue-anygantt/any-gantt/src/components/AnyGantt.vue
@@ -4,6 +4,7 @@
 
 <script>
 import anychart from 'anychart';
+import axios from 'axios';
 export default {
 	name: 'AnyGantt',
 	mounted: function() {
@@ -13,48 +14,110 @@ export default {
 		// Declares the method
 		gantt: function() {
 			// create data
-			var data = [
+			// var data = [
+			// 	{
+			// 		id: '1',
+			// 		name: 'Development1',
+			// 		actualStart: '2018-01-15',
+			// 		actualEnd: '2018-03-10',
+			// 		children: [
+			// 			{
+			// 				id: '1_1',
+			// 				name: 'Analysis',
+			// 				actualStart: '2018-01-15',
+			// 				actualEnd: '2018-01-25',
+			// 			},
+			// 			{
+			// 				id: '1_2',
+			// 				name: 'Design',
+			// 				actualStart: '2018-01-20',
+			// 				actualEnd: '2018-02-04',
+			// 			},
+			// 			{
+			// 				id: '1_3',
+			// 				name: 'Meeting',
+			// 				actualStart: '2018-02-05',
+			// 				actualEnd: '2018-02-05',
+			// 			},
+			// 			{
+			// 				id: '1_4',
+			// 				name: 'Implementation',
+			// 				actualStart: '2018-02-05',
+			// 				actualEnd: '2018-02-24',
+			// 			},
+			// 			{
+			// 				id: '1_5',
+			// 				name: 'Testing',
+			// 				actualStart: '2018-02-25',
+			// 				actualEnd: '2018-03-10',
+			// 			},
+			// 		],
+			// 	},
+			// ];
+
+
+			// let url = 'http://51.83.129.102:8000/graphql/';
+			let url = 'http://localhost:8000/graphql/';
+			var data = [];
+			// let url = 'http://backend:8000/graphql/';
+
+			axios.post(url, {
+			query: `
 				{
-					id: '1',
-					name: 'Development1',
-					actualStart: '2018-01-15',
-					actualEnd: '2018-03-10',
-					children: [
-						{
-							id: '1_1',
-							name: 'Analysis',
-							actualStart: '2018-01-15',
-							actualEnd: '2018-01-25',
-						},
-						{
-							id: '1_2',
-							name: 'Design',
-							actualStart: '2018-01-20',
-							actualEnd: '2018-02-04',
-						},
-						{
-							id: '1_3',
-							name: 'Meeting',
-							actualStart: '2018-02-05',
-							actualEnd: '2018-02-05',
-						},
-						{
-							id: '1_4',
-							name: 'Implementation',
-							actualStart: '2018-02-05',
-							actualEnd: '2018-02-24',
-						},
-						{
-							id: '1_5',
-							name: 'Testing',
-							actualStart: '2018-02-25',
-							actualEnd: '2018-03-10',
-						},
-					],
-				},
-			];
+					baseline (id:110) {
+					id
+					name
+					belongsTo {
+						id
+					}
+					projects {
+						wbs
+						project {
+						id
+						name
+						}
+						start
+						finish
+						belongsTo {
+						id
+						}
+						predecessorsIdFS
+					}
+					}
+				}
+				`
+			}).then((response) => { 
+			let projects = response['data']['data']['baseline']['projects']; 
+
+			for (let i = 0; i < projects.length; i++) {
+				let task = {
+					id: projects[i]['project']['id'].toString(),
+					name: projects[i]['project']['name'],
+					// user: '<a href="https://images.pexels.com/photos/423364/pexels-photo-423364.jpeg?auto=compress&cs=tinysrgb&h=650&w=940" target="_blank" style="color:#0077c0;">Awesome!</a>',
+					actualStart: projects[i]['start'],
+					actualEnd: projects[i]['finish']
+					// duration: moment(projects[i]['finish']).diff(moment(projects[i]['start'])),
+					// percent: 0,
+					// type: "task"
+				}
+
+				if (projects[i]['belongsTo']['id'] != response['data']['data']['baseline']['belongsTo']['id']) { 
+					task['parent'] = projects[i]['belongsTo']['id'].toString()
+				}
+
+				// task['dependentOn'] = projects[i]['predecessorsIdFS']
+
+
+				data.push(task);
+			}
+
+			
+			});
+
+			console.log(data);
+
 			// create a data tree
-			var treeData = anychart.data.tree(data, 'as-tree');
+			var treeData = anychart.data.tree(data, 'as-table');
 			// create a chart
 			var chart = anychart.ganttProject();
 			// set the data
